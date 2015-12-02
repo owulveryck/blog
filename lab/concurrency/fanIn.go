31a692b4091284300eaecdb5c71de083367902b9
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2015-12-02 22:30:44 +0100

    WIP

--- lab/concurrency/fanIn.go
+++ lab/concurrency/fanIn.go
@@ -6,40 +6,51 @@ import (
 	"time"
 )
 
+type Message struct {
+	str  string
+	wait chan bool
+}
+
 func main() {
-	n := 11
-	cs := make([]<-chan string, n)
+	n := 3
+	cs := make([]<-chan Message, n)
 	for i := 0; i < n; i++ {
 		cs[i] = boring(fmt.Sprintf("Boring:%v", i))
 	}
+
 	c := fanIn(cs...)
 	for i := 0; i < n; i++ {
-		fmt.Println(<-c)
+		msg := <-c
+		msg.wait <- true
+		fmt.Println(msg.str)
+
 	}
 	fmt.Println("This is the end!")
 }
 
-func boring(msg string) <-chan string {
-	c := make(chan string)
+func boring(msg string) <-chan Message {
+	c := make(chan Message)
+	waitForIt := make(chan bool) // Shared between all messages.
 	go func() {
-		time.Sleep(time.Duration(rand.Intn(1e3)) * 3 * time.Millisecond)
-		c <- fmt.Sprintf("%v", msg)
+		for i := 0; ; i++ {
+			c <- Message{fmt.Sprintf("%s: %d", msg, i), waitForIt}
+			fmt.Printf("%v: %d WAITING\n", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+			fmt.Printf("%v: %d DONE\n", msg, i)
+			<-waitForIt
+		}
 	}()
 	return c
 }
-
-func fanIn(input ...<-chan string) <-chan string {
-	c := make(chan string)
-	go func() {
-		for {
-			for _, n := range input {
-				select {
-				case s := <-n:
-					c <- s
-				default:
-				}
+func fanIn(inputs ...<-chan Message) <-chan Message { // HL
+	c := make(chan Message)
+	for i := range inputs {
+		input := inputs[i] // New instance of 'input' for each loop.
+		go func() {
+			for {
+				c <- <-input
 			}
-		}
-	}()
+		}()
+	}
 	return c
 }
