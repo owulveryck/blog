2730500f3797a1514d0fe72cb9bda7f61c75d404
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2015-12-07 14:28:51 +0000

    Structure does not have inputs anymore, it is replaced by the args

--- lab/oaas/structure.go
+++ lab/oaas/structure.go
@@ -17,7 +17,6 @@ type Node struct {
 	Engine   string            `json:engine:omitempty` // The execution engine (ie ansible, shell); aim to be like a shebang in a shell file
 	Artifact string            `json:artifact`
 	Args     []string          `json:args,omitempty`   // the arguments of the artifact, if needed
-	Inputs   map[string]string `json:input,omitempty`  // the key is the node that gives the information, and  the value, its output (always a string)
 	Outputs  map[string]string `json:output,omitempty` // the key is the name of the parameter, the value its value (always a string)
 }
 
@@ -33,23 +32,24 @@ func main() {
 			0, 0, 0, 0, 0, 0, 1, 0,
 		},
 		[]Node{
-			{0, "a", "ansible", "myplaybook.yml", nil, nil, nil},
-			{1, "b", "ansible", "myplaybook1.yml", nil, nil,
+			{0, "a", "ansible", "myplaybook.yml", nil, nil},
+			{1, "b", "ansible", "myplaybook1.yml", nil,
 				map[string]string{
 					"output1": "",
 				},
 			},
-			{2, "c", "ansible", "myplaybook2.yml", nil,
-				map[string]string{
-					"b": "output1",
+			{2, "c", "ansible", "myplaybook2.yml",
+				[]string{
+					"-e", "get_attribute 1:output1",
 				}, nil},
-			{3, "d", "ansible", "myplaybook3.yml", nil, nil, nil},
-			{4, "e", "ansible", "myplaybook4.yml", nil, nil, nil},
-			{5, "f", "ansible", "myplaybook5.yml", nil, nil, nil},
-			{6, "g", "ansible", "myplaybook6.yml", nil, nil, nil},
-			{7, "h", "ansible", "myplaybook7.yml", nil, nil, nil},
+			{3, "d", "ansible", "myplaybook3.yml", nil, nil},
+			{4, "e", "ansible", "myplaybook4.yml", nil, nil},
+			{5, "f", "ansible", "myplaybook5.yml", nil, nil},
+			{6, "g", "ansible", "myplaybook6.yml", nil, nil},
+			{7, "h", "ansible", "myplaybook7.yml", nil, nil},
 		},
 	}
+	fmt.Println(test)
 	o, err := json.MarshalIndent(test, "  ", "  ")
 	if err != nil {
 		panic(err)
