fe948f5e8e65bb81045ecaa4134fcc2d0197d5c2
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2015-12-07 13:39:46 +0000

    The structure seems complete

--- lab/oaas/structure.go
+++ lab/oaas/structure.go
@@ -11,11 +11,13 @@ type Input struct {
 }
 
 type Node struct {
-	ID       int      `json:id`
-	Name     string   `json:name,omitempty`
-	Engine   string   `json:engine:omitempty` // The execution engine (ie ansible, shell); aim to be like a shebang in a shell file
-	Artifact string   `json:artifact`
-	Args     []string `json:args`
+	ID       int               `json:id`
+	Name     string            `json:name,omitempty`
+	Engine   string            `json:engine:omitempty` // The execution engine (ie ansible, shell); aim to be like a shebang in a shell file
+	Artifact string            `json:artifact`
+	Args     []string          `json:args,omitempty`   // the arguments of the artifact, if needed
+	Inputs   map[string]string `json:input,omitempty`  // the key is the node that gives the information, and  the value, its output (always a string)
+	Outputs  map[string]string `json:output,omitempty` // the key is the name of the parameter, the value its value (always a string)
 }
 
 func main() {
